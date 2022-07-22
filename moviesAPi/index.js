69372aca7ed046df0fc36dddb58c81dfe2fb35c9
Dev Chauhan <chauhandev136@gmail.com>
2022-07-22 15:43:04 +0530

    del

--- moviesAPi/index.js
+++
@@ -1,5 +0,0 @@
-var url = `http://www.omdbapi.com/?apikey=f110fe32&s=batman`;
-fetch(url)
-.then((success) => {success.json()})
-.then((movies) =>{ console.log(movies)})
-.catch((error) => console.log(error))
